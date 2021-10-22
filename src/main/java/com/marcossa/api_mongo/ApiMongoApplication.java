1d3481045d1e29b91141ddd2cb4e4f1d25a91dc7
vrsmarcos00 <vrr.marcos@gmail.com>
2021-10-22 17:58:23 -0300

    Initial Project

---
+++ src/main/java/com/marcossa/api_mongo/ApiMongoApplication.java
@@ -0,0 +1,13 @@
+package com.marcossa.api_mongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiMongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marcossa/api_mongo/ApiMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcossa.api_mongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
