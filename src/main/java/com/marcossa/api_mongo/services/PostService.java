f0e24195b541ef4853cf5ab6f7c254a5de1d222c
vrsmarcos00 <vrr.marcos@gmail.com>
2021-10-26 16:33:07 -0300

    Obtendo um post por ID

---
+++ src/main/java/com/marcossa/api_mongo/services/PostService.java
@@ -0,0 +1,28 @@
+package com.marcossa.api_mongo.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.marcossa.api_mongo.domain.Post;
+import com.marcossa.api_mongo.repository.PostRepository;
+import com.marcossa.api_mongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repository;
+	
+	public List<Post> findAll(){
+		return repository.findAll();
+	}
+	
+	public Post findById(String id) {
+		Optional<Post> posts = repository.findById(id);
+		return posts.orElseThrow(() -> new ObjectNotFoundException(id));
+	}
+
+}
