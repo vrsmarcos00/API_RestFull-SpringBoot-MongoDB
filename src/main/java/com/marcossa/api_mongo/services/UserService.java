bba9e2709fe79ab86e0fa614784d52e1c6e0130e
vrsmarcos00 <vrr.marcos@gmail.com>
2021-10-26 13:56:22 -0300

    Inserção de usuário com Post

--- src/main/java/com/marcossa/api_mongo/services/UserService.java
+++ src/main/java/com/marcossa/api_mongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.marcossa.api_mongo.domain.User;
+import com.marcossa.api_mongo.dto.UserDTO;
 import com.marcossa.api_mongo.repository.UserRepository;
 import com.marcossa.api_mongo.services.exception.ObjectNotFoundException;
 
@@ -23,5 +24,13 @@ public class UserService  {
 	 public User findById(String id) {
 		 Optional<User> user = repository.findById(id);
 		 return user.orElseThrow(() -> new ObjectNotFoundException("Usuário não encontrado"));
+	 }
+	 
+	 public User insert(User obj) {
+		 return repository.insert(obj);
+	 }
+	 
+	 public User fromDTO(UserDTO objDto) {
+		 return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	 }
 }
