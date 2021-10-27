e1f6cfa0bfe9990d3331d65b4fb6a178521214ed
vrsmarcos00 <vrr.marcos@gmail.com>
2021-10-27 13:23:08 -0300

    Consulta com vários critérios

--- src/main/java/com/marcossa/api_mongo/services/PostService.java
+++ src/main/java/com/marcossa/api_mongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.marcossa.api_mongo.services;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -27,6 +28,11 @@ public class PostService {
 	
 	public List<Post> findByTitle(String text){
 		return repository.findByTitle(text);
+	}
+	
+	public List<Post> fullSearch(String text, Date minDate, Date maxDate){
+		maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+		return repository.fullSearch(text, minDate, maxDate);
 	}
 
 }
