bdec23f10dff28281d109343650d1ab531fbcbc3
vrsmarcos00 <vrr.marcos@gmail.com>
2021-10-27 11:20:03 -0300

    Consulta simples com @Query

--- src/main/java/com/marcossa/api_mongo/repository/PostRepository.java
+++ src/main/java/com/marcossa/api_mongo/repository/PostRepository.java
@@ -3,12 +3,16 @@ package com.marcossa.api_mongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.marcossa.api_mongo.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+	
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> findByTitle(String text);
 	
 	List<Post> findByTitleContainingIgnoreCase(String Text);
 	
